cdc375cec98ce18adf4ac582f99d672b0b4abf22
gotanks <gotanksgn@163.com>
2020-04-04 09:56:18 +0800

    更改alisv初始化位置

--- uni_alisv/src/main/java/com/gotanks/uni_alisv/AliSvAppHookProxy.java
+++ uni_alisv/src/main/java/com/gotanks/uni_alisv/AliSvAppHookProxy.java
@@ -1,21 +1,22 @@
-package com.jagt.viewinter1.application;
+package com.gotanks.uni_alisv;
+
+import android.app.Application;
 
 import com.aliyun.common.httpfinal.QupaiHttpFinal;
 import com.aliyun.sys.AlivcSdkCore;
 import com.gotanks.uni_alisv.common.LicenseInterfaceHook;
 import com.gotanks.uni_alisv.recorder.faceunity.FaceUnityManager;
 
-import io.dcloud.application.DCloudApplication;
+import io.dcloud.weex.AppHookProxy;
 
-public class MyApplication extends DCloudApplication {
+public class AliSvAppHookProxy implements AppHookProxy {
+
     @Override
-    public void onCreate() {
-        super.onCreate();
-
-        LicenseInterfaceHook.init(getApplicationContext());
-        FaceUnityManager.getInstance().setUp(this);
+    public void onCreate(Application application) {
+        LicenseInterfaceHook.init(application.getApplicationContext());
+        FaceUnityManager.getInstance().setUp(application);
         //短视频sdk，暂时只支持api 18以上的版本
-        AlivcSdkCore.register(getApplicationContext());
+        AlivcSdkCore.register(application.getApplicationContext());
         //LittleHttpManager.getInstance().init(this);
 
         QupaiHttpFinal.getInstance().initOkHttpFinal();
